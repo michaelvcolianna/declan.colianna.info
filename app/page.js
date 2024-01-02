fc018b761a58275a9cf23030500616dfed5833ea
Michael V. Colianna <michael.colianna@me.com>
2024-01-02 15:25:30 -0500

    Updates page info

--- app/page.js
+++ app/page.js
@@ -3,8 +3,20 @@ import Image from 'next/image'
 const ExternalLink = ({ href, children }) => {
   return (
     <li>
-      <a href={href} className="justify-center flex flex-row gap-2 items-center underline">{children} <svg aria-label="Opens an external site" className="h-4 w-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg></a>
+      <a href={href} className="text-indigo-700 flex flex-row flex-wrap gap-2 items-center underline text-wrap">{children}</a>
     </li>
+  )
+}
+
+const ContentBlock = ({ children }) => {
+  return (
+    <div className="text-3xl grid gap-6">{children}</div>
+  )
+}
+
+const InfoList = ({ children }) => {
+  return (
+    <ul className="flex flex-col lg:flex-row gap-8 text-3xl w-full">{children}</ul>
   )
 }
 
@@ -12,15 +24,18 @@ export default function Home() {
   const updated = new Date().toLocaleString('en', { timeZone: 'America/New_York' })
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-8 lg:p-24 gap-8">
-      <div className="text-sm italic lg:-mt-8">Last updated: {updated} US Eastern time</div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-8 lg:p-24 gap-8 text-xl">
+      <div className="text-slate-700 w-full text-sm italic lg:-mt-8">Last updated: {updated} US Eastern time</div>
 
-      <div className="font-bold text-6xl">Declan Wilder Colianna</div>
+      <div className="font-bold">
+        <span className="block">In loving memory:</span>
+        <span className="text-6xl">Declan Wilder Colianna</span>
+      </div>
 
       <div className="z-1">
         <Image
           src="/decky.jpg"
-          alt="Declan, looking pensively out of a window"
+          alt=""
           className="h-auto w-96"
           width={1024}
           height={1365}
@@ -28,20 +43,41 @@ export default function Home() {
         />
       </div>
 
-      <div className="text-3xl grid gap-6">
+      <ContentBlock>
+        <p>On December 27th, 2023, at some point between 9:40pm and 11:00pm US Eastern time, Declan's heart stopped. Lani and Michael found him unresponsive, and despite the best efforts of the EMTs, they weren't able to resuscitate him.</p>
         <p>Declan was born on August 3rd, 2023 at 8:47am US Eastern time.</p>
         <p>It took seven years of IVF for Declan's parents, Lani and Michael, to get him here. When he first showed up, he was one of two babies – twins – but unfortunately his sibling died suddenly in utero.</p>
-        <p>On December 27th, 2023, at some point between 9:40pm and 11:00pm US Eastern time, Declan's heart stopped. Lani and Michael found him unresponsive, and despite the best efforts of the EMTs, they weren't able to resuscitate him.</p>
-        <p>In his short time here, Declan touched so many lives – even those of people he didn't meet. So his family created this page in his memory. Below are some links to further information.</p>
-      </div>
+        <p>In his short time here, Declan touched so many lives – even those of people he didn't meet. So his family created this page in his memory.</p>
+        <p>Below are some links to further information about what happened, along with details for family and friends traveling for the service. (All open external websites.)</p>
+      </ContentBlock>
 
-      <ul className="flex flex-col lg:flex-row justify-center gap-8">
+      <InfoList>
         <ExternalLink href="https://www.instagram.com/p/C1hI24CuCoU/?igsh=c29sMjUxMHY3ajI%3D">Lani's Instagram Post</ExternalLink>
         <ExternalLink href="https://www.facebook.com/michaelcolianna/posts/pfbid0271Dx14KKCbyQnXFoT9dLjKpRSgVBmLWMCTJUEd1nrhXgbVHqEt341sVzbyjuCJwGl">Michael's Facebook Post</ExternalLink>
         <ExternalLink href="https://www.gofundme.com/f/in-loving-memory-of-declan-wilder-colianna">GoFundMe Page</ExternalLink>
+        <ExternalLink href="https://www.ihg.com/holidayinnexpress/hotels/us/en/branford/bfact/hoteldetail">Holiday Inn Express (Branford)</ExternalLink>
+        <ExternalLink href="https://www.wyndhamhotels.com/baymont/branford-connecticut/baymont-inn-and-suites-branford-new-haven/overviewhoteldetail">Baymont by Wyndham (Branford)</ExternalLink>
+        <ExternalLink href="https://www.omnihotels.com/hotels/new-haven-yale">The Omni Hotel (New Haven)</ExternalLink>
+        <ExternalLink href="https://www.newhavenhotel.com/">New Haven Hotel (New Haven)</ExternalLink>
+        <ExternalLink href="https://www.theblakenewhaven.com/">The Blake Hotel (New Haven)</ExternalLink>
         {/* <ExternalLink href="#">Obituary</ExternalLink>
         <ExternalLink href="#">Live Stream</ExternalLink> */}
-      </ul>
+      </InfoList>
+
+      <ContentBlock>
+        <p>Declan's family is sensitive to flowers, so in lieu, a simple message of sympathy is fine. If you'd like to make a donation in Declan's name, his family has provided a list of organizations that mean a lot to them. (All open external websites.)</p>
+      </ContentBlock>
+
+      <InfoList>
+        <ExternalLink href="https://www.stjude.org/">St. Jude Children's Research Hospital</ExternalLink>
+        <ExternalLink href="https://resolve.org/">Resolve (The National Infertility Association)</ExternalLink>
+        <ExternalLink href="https://www.sandyhookpromise.org/">Sandy Hook Promise</ExternalLink>
+        <ExternalLink href="https://www.hearingloss.org/">Hearing Loss Association of America</ExternalLink>
+      </InfoList>
+
+      <div className="pb-8 w-full">
+        <a href="mailto:declan@colianna.info" className="underline text-indigo-700 font-bold">Contact the family</a>
+      </div>
     </main>
   )
 }

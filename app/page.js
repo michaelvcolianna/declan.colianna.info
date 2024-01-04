50c4f64a54a70f123a623fcc5e414228accc6bcf
Michael V. Colianna <michael.colianna@me.com>
2024-01-04 10:11:04 -0500

    Organizes information into disclosure sections

--- app/page.js
+++ app/page.js
@@ -14,6 +14,34 @@ const ExternalLink = ({ href, children }) => {
   )
 }
 
+const ExternalText = () => {
+  return (
+    <>(All open external websites.)</>
+  )
+}
+
+const DisclosureBlock = ({ anchor, summary, children }) => {
+  return (
+    <div className="w-full">
+      <details id={anchor} className="border-2 border-dashed border-black p-4 [&_svg]:open:-rotate-45">
+        <summary className="flex cursor-pointer list-none items-center gap-4">
+          <div>
+            <svg className="rotate-0 transform text-black transition-all duration-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+          </div>
+
+          <div className="font-bold text-2xl">
+            {summary}
+          </div>
+        </summary>
+
+        <div className="p-4">
+          {children}
+        </div>
+      </details>
+    </div>
+  )
+}
+
 const ContentBlock = ({ children }) => {
   return (
     <div className="text-3xl grid gap-6">{children}</div>
@@ -22,7 +50,7 @@ const ContentBlock = ({ children }) => {
 
 const InfoList = ({ children }) => {
   return (
-    <ul className="flex flex-col lg:grid grid-cols-2 gap-8 text-3xl w-full">{children}</ul>
+    <ul className="mt-8 flex flex-col lg:grid grid-cols-2 gap-8 text-3xl w-full">{children}</ul>
   )
 }
 
@@ -31,7 +59,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-8 lg:p-24 gap-8 text-xl">
-      <div className="text-slate-700 w-full text-sm italic lg:-mt-8">Last updated: {updated} US Eastern time: Added information about Declan's service.</div>
+      <div className="text-slate-700 w-full text-sm italic lg:-mt-8">Last updated: {updated} US Eastern time: Organized the information better.</div>
 
       <div className="font-bold w-full">
         <span className="block">In loving memory:</span>
@@ -51,40 +79,52 @@ export default function Home() {
 
       <ContentBlock>
         <p>On December 27th, 2023, at some point between 9:40pm and 11:00pm US Eastern time, Declan's heart stopped. Lani and Michael found him unresponsive, and despite the best efforts of the EMTs, they weren't able to resuscitate him.</p>
-        <p>Declan was born on August 3rd, 2023 at 8:47am US Eastern time.</p>
-        <p>It took seven years of IVF for Declan's parents, Lani and Michael, to get him here. When he first showed up, he was one of two babies – twins – but unfortunately his sibling died in utero.</p>
-        <p>In his short time here, Declan touched so many lives – even those of people he didn't meet. So his family created this page in his memory.</p>
         <p className="font-bold">Friends and family are invited to services on January 20th, 2024 at 11:00am, at <Link href="https://www.google.com/maps/dir//41.291014,-72.8053558/@41.290994,-72.8877291,12z?entry=ttu">W.S. Clancy Funeral Home</Link>, at 244 North Main Street, Branford, CT, 06405.</p>
         <p className="italic">Reception information will be posted soon.</p>
       </ContentBlock>
 
-      <ContentBlock>
-        <p>Below are some links to further information about what happened, along with details for family and friends traveling for the service. (All open external websites.)</p>
-      </ContentBlock>
+      <DisclosureBlock anchor="about-declan" summary="About Declan">
+        <ContentBlock>
+          <p>Declan was born on August 3rd, 2023 at 8:47am US Eastern time.</p>
+          <p>It took seven years of IVF for Declan's parents, Lani and Michael, to get him here. When he first showed up, he was one of two babies – twins – but unfortunately his sibling died in utero.</p>
+          <p>In his short time here, Declan touched so many lives – even those of people he didn't meet. So his family created this page in his memory.</p>
+          <p>Below are some links with further information about what happened. <ExternalText /></p>
+        </ContentBlock>
 
-      <InfoList>
-        {/* <ExternalLink href="https://www.instagram.com/p/C1hI24CuCoU/?igsh=c29sMjUxMHY3ajI%3D">Lani's Instagram Post</ExternalLink> */}
-        <ExternalLink href="https://www.facebook.com/michaelcolianna/posts/pfbid0271Dx14KKCbyQnXFoT9dLjKpRSgVBmLWMCTJUEd1nrhXgbVHqEt341sVzbyjuCJwGl">Michael's Facebook Post</ExternalLink>
-        <ExternalLink href="https://www.gofundme.com/f/in-loving-memory-of-declan-wilder-colianna">GoFundMe Page</ExternalLink>
-        <ExternalLink href="https://www.ihg.com/holidayinnexpress/hotels/us/en/branford/bfact/hoteldetail">Holiday Inn Express (Branford)</ExternalLink>
-        <ExternalLink href="https://www.wyndhamhotels.com/baymont/branford-connecticut/baymont-inn-and-suites-branford-new-haven/overviewhoteldetail">Baymont by Wyndham (Branford)</ExternalLink>
-        <ExternalLink href="https://www.omnihotels.com/hotels/new-haven-yale">The Omni Hotel (New Haven)</ExternalLink>
-        <ExternalLink href="https://www.newhavenhotel.com/">New Haven Hotel (New Haven)</ExternalLink>
-        <ExternalLink href="https://www.theblakenewhaven.com/">The Blake Hotel (New Haven)</ExternalLink>
-        {/* <ExternalLink href="#">Obituary</ExternalLink>
-        <ExternalLink href="#">Live Stream</ExternalLink> */}
-      </InfoList>
+        <InfoList>
+          <ExternalLink href="https://www.facebook.com/michaelcolianna/posts/pfbid0271Dx14KKCbyQnXFoT9dLjKpRSgVBmLWMCTJUEd1nrhXgbVHqEt341sVzbyjuCJwGl">Michael's Facebook Post</ExternalLink>
+          <ExternalLink href="https://www.gofundme.com/f/in-loving-memory-of-declan-wilder-colianna">GoFundMe Page</ExternalLink>
+          {/* <ExternalLink href="#">Obituary</ExternalLink>
+          <ExternalLink href="#">Live Stream</ExternalLink> */}
+        </InfoList>
+      </DisclosureBlock>
 
-      <ContentBlock>
-        <p>Declan's family is sensitive to flowers, so in lieu, a simple message of sympathy is fine. If you'd like to make a donation in Declan's name, his family has provided a list of organizations that mean a lot to them. (All open external websites.)</p>
-      </ContentBlock>
+      <DisclosureBlock anchor="travel-accommodations" summary="Travel & Accommodations">
+        <ContentBlock>
+          <p>Below are some links for family and friends traveling for the service. <ExternalText /></p>
+        </ContentBlock>
 
-      <InfoList>
-        <ExternalLink href="https://resolve.org/">Resolve (The National Infertility Association)</ExternalLink>
-        <ExternalLink href="https://www.stjude.org/">St. Jude Children's Research Hospital</ExternalLink>
-        <ExternalLink href="https://www.sandyhookpromise.org/">Sandy Hook Promise</ExternalLink>
-        <ExternalLink href="https://www.hearingloss.org/">Hearing Loss Association of America</ExternalLink>
-      </InfoList>
+        <InfoList>
+          <ExternalLink href="https://www.ihg.com/holidayinnexpress/hotels/us/en/branford/bfact/hoteldetail">Holiday Inn Express (Branford)</ExternalLink>
+          <ExternalLink href="https://www.wyndhamhotels.com/baymont/branford-connecticut/baymont-inn-and-suites-branford-new-haven/overviewhoteldetail">Baymont by Wyndham (Branford)</ExternalLink>
+          <ExternalLink href="https://www.omnihotels.com/hotels/new-haven-yale">The Omni Hotel (New Haven)</ExternalLink>
+          <ExternalLink href="https://www.newhavenhotel.com/">New Haven Hotel (New Haven)</ExternalLink>
+          <ExternalLink href="https://www.theblakenewhaven.com/">The Blake Hotel (New Haven)</ExternalLink>
+        </InfoList>
+      </DisclosureBlock>
+
+      <DisclosureBlock anchor="sympathy-donations" summary="Sympathy & Donations">
+        <ContentBlock>
+          <p>Declan's family is sensitive to flowers, so in lieu, a simple message of sympathy is fine. If you'd like to make a donation in Declan's name, his family has provided a list of organizations that mean a lot to them. <ExternalText /></p>
+        </ContentBlock>
+
+        <InfoList>
+          <ExternalLink href="https://resolve.org/">Resolve (The National Infertility Association)</ExternalLink>
+          <ExternalLink href="https://www.stjude.org/">St. Jude Children's Research Hospital</ExternalLink>
+          <ExternalLink href="https://www.sandyhookpromise.org/">Sandy Hook Promise</ExternalLink>
+          <ExternalLink href="https://www.hearingloss.org/">Hearing Loss Association of America</ExternalLink>
+        </InfoList>
+      </DisclosureBlock>
 
       <div className="pb-8 w-full lg:pt-8">
         <a href="mailto:declan@colianna.info" className="underline text-indigo-700 font-bold">Contact the family</a>

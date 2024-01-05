49b6c8dc3deda4ba1a0da6981cf7d759aec2fdae
Michael V. Colianna <12972792+michaelvcolianna@users.noreply.github.com>
2024-01-05 05:40:09 -0500

    Update page.js
    SVG change

--- app/page.js
+++ app/page.js
@@ -26,7 +26,7 @@ const DisclosureBlock = ({ anchor, summary, children }) => {
       <details id={anchor} className="border-2 border-dashed border-black p-4 [&_svg]:open:-rotate-45">
         <summary className="flex cursor-pointer list-none items-center gap-4">
           <div>
-            <svg className="rotate-0 transform text-black transition-all duration-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+            <svg className="hidden lg:inline rotate-0 transform text-black transition-all duration-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
           </div>
 
           <div className="font-bold text-2xl">
